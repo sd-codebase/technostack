939f75e334c87e7da5ff15bf59817fa3e17a17db
Krishna Suryavanshi <krishna.dhas021815@gmail.com>
2022-10-11 20:40:21 +0530

    feat: angular service and web worker

---
+++ Angular/service worker.ts
@@ -0,0 +1,23 @@
+Service worker
+A service worker is a script that runs in the web browser and manages caching for an application.
+Service workers work as a network proxy.
+Adding a service worker to an Angular application is one of the steps for turning an application into a Progressive Web App
+
+Adding a service worker to project
+    ng add @angular/pwa
+    - Adds the @angular/service-worker package to your project.
+    - Enables service worker build support in the CLI.
+    - Imports and registers the service worker in the application module.
+    - Updates the index.html file:
+        Includes a link to add the manifest.webmanifest file
+        Adds a meta tag for theme-color
+    - Installs icon files to support the installed Progressive Web App (PWA).
+    - Creates the service worker configuration file called ngsw-config.json, which specifies the caching behaviors and other settings.
+Build project
+Run PWA supported angular app from dist folder
+Following thinf will be cached
+- index.html
+- favicon.ico
+- Build artifacts (JS and CSS bundles)
+- Anything under assets
+- Images and fonts directly under the configured outputPath
---
+++ Angular/web workers.ts
@@ -0,0 +1,94 @@
+Web workers
+We run CPU-intensive computations in a background thread,
+ this way we can free main thread to user interface
+Using web workers can help increase your application's performance
+
+Adding a web worker
+    ng generate web-worker <location>
+    eg. ng generate web-worker app
+    1. This command configures your project to use web workers
+        - CREATE tsconfig.worker.json
+        - CREATE src/app/app.worker.ts
+        - UPDATE tsconfig.app.json
+        - UPDATE angular.json
+        - UPDATE src/app/app.component.ts
+    2. Adds tsconfig.worker.json
+            // tsconfig.worker.json
+            {
+                "extends": "./tsconfig.json",
+                "compilerOptions": {
+                    "outDir": "./out-tsc/worker",
+                    "lib": [
+                        "es2018",
+                        "webworker"
+                    ],
+
+
+                    "types": []
+                },
+                "include": [
+                    "src/**/*.worker.ts"
+                ]
+            }
+
+    3.tsconfig.worker.json extends tsconfig.json and includes options to compile web workers.
+            // tsconfig.app.json [only a snippet]
+            "exclude": [
+            "src/test.ts",
+            "src/**/*.spec.ts",
+            "src/**/*.worker.ts"
+            ]
+
+    4. it excludes all the worker from compiling as it has separate configuration.
+            // angular.json (only a snippet) 
+            "webWorkerTsConfig": "tsconfig.worker.json"
+
+
+    5. Adds the following code to src/app/app.worker.ts to receive messages.
+        addEventListener('message', ({ data }) => {
+            const response = `worker response to ${data}`;
+            postMessage(response);
+        });
+
+
+    6. Adds the following code to src/app/app.component.ts to use the worker.
+        if (typeof Worker !== 'undefined') {
+            // Create a new
+            const worker = new Worker(new URL('./app.worker', import.meta.url));
+            worker.onmessage = ({ data }) => {
+                console.log(`page got message: ${data}`);
+            };
+            worker.postMessage('hello');
+        } else {
+            // Web workers are not supported in this environment.
+            // You should add a fallback so that your program still executes correctly.
+        }
+
+
+You have to refactor your code to use the web worker.
+
+Full working example
+- Create Prime calculator to calculate prime numbers
+- Update web worker to calculate prime number
+    addEventListener('message', ({ data }) => {
+        // const response = `worker response to ${data}`;
+        const response = PrimeCalculator.findNthPrimeNumber(parseInt(data));
+        postMessage(response);
+    });
+- In app component
+    findnthPrimeNumber(num) {
+      if (typeof Worker !== 'undefined') {
+         // Create a new
+         const worker = new Worker('./app.worker', { type: 'module' });
+         worker.onmessage = ({ data }) => {
+            this.primeNum = data;
+         };
+         worker.postMessage(num);
+      } else {
+         // Web Workers are not supported in this environment.
+         // You should add a fallback so that your program still executes correctly.
+      }
+   }
+
+- In template
+<a href="#" (click)="findnthPrimeNumber(10000)">Click here</a> to find 10000th prime number
